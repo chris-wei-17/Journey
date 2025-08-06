0759c00cd92b3a50b889d93a3b1fbc4bea1ff9af
Chris Wei <43208040+chris-wei-17@users.noreply.github.com>
2025-08-06 15:48:35 -0400

    Update metrics-block.tsx

--- client/src/components/metrics-block.tsx
+++ client/src/components/metrics-block.tsx
@@ -253,7 +253,7 @@ export function MetricsBlock({ selectedDate }: MetricsBlockProps) {
                 ADD CUSTOM METRIC
               </Button>
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
               <DialogHeader>
                 <DialogTitle>Add Custom Metric</DialogTitle>
               </DialogHeader>
